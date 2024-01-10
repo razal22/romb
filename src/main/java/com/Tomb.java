afd5163c143d829d4fa5c9466b60b5f09ece4caa
unknown <nagy.berta.lazar@gmail.com>
2024-01-10 11:01:21 +0100

    6

--- src/main/java/com/Tomb.java
+++ src/main/java/com/Tomb.java
@@ -1,7 +1,16 @@
 package com;
+
+import java.util.InputMismatchException;
+
 public class Tomb {
     public double CalcTombTer(double side, double alfa){
+        if (side <= 0 || alfa <=0){
+            throw new InputMismatchException();
+        }
         double rad = alfa*Math.PI/180;
         return(side*side*Math.sin(rad));
+    }
+    public double CalcTombKer(double side, double alfa){
+        return(side*4);
     }
 }
--- src/test/java/TombTest.java
+++ src/test/java/TombTest.java
@@ -1,4 +1,6 @@
 
+import java.util.InputMismatchException;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -23,5 +25,27 @@ public void setUp(){
         double Kactual = this.tomb.CalcTombKer(8,30);
         double Kexpected = 32;
         Assert.assertEquals(Kactual, Kexpected,0.1);
+    }
+    @Test
+    public void testCalcTomb_6_40(){
+        double actual = this.tomb.CalcTombTer(6,40);
+        double expected = 23.1;
+        Assert.assertEquals(actual, expected,0.1);
+
+        double Kactual = this.tomb.CalcTombKer(6,40);
+        double Kexpected = 24;
+        Assert.assertEquals(Kactual, Kexpected,0.1);
+    }
+    @Test(expectedExceptions = InputMismatchException.class)
+    public void testCalcTomb_Zero_Side(){
+        this.tomb.CalcTombKer(0, 1);
+        this.tomb.CalcTombTer(0, 1);
+
+    }
+    @Test(expectedExceptions = InputMismatchException.class)
+    public void testCalcRadius_Side_Zero(){
+        this.tomb.CalcTombKer(1, 0);
+        this.tomb.CalcTombTer(1, 0);
+
     }
 }
