5956f632db0b8403cfa3f5c77d5c5410a379b895
unknown <nagy.berta.lazar@gmail.com>
2024-01-10 10:37:34 +0100

    3

--- src/test/java/TombTest.java
+++ src/test/java/TombTest.java
@@ -1,5 +1,8 @@
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import com.Tomb;
 
 public class TombTest {
@@ -10,3 +13,11 @@ public void setUp(){
     this.tomb = new Tomb();
 }
 }
+
+    @Test
+    public void testCalcTomb_8_30_40(){
+        double actual = this.tomb.CalcTomb(8,30,40);
+        double expected = 41.1;
+        Assert.assertEquals(actual, expected,0.1);
+    }
+    
